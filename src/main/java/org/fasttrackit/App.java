aa506e73db89dea6d2d8df66ffc4534956af0b29
cioroiuradu <cioroiu.radu@gmail.com>
2019-01-16 21:19:57 +0200

    add collections

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -11,6 +11,10 @@ public class App {
     public static void main(String[] args) {
 
         Game game = new Game();
+
+        game.addCompetitors(2);
+        game.displayCompetitors();
+
         game.addTrack(0, new Track("Highway", 300));
         game.addTrack(1, new Track("Seaside", 100));
 
--- src/main/java/org/fasttrackit/Game.java
+++ src/main/java/org/fasttrackit/Game.java
@@ -1,8 +1,29 @@
 package org.fasttrackit;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Game {
 
     private Track[] tracks = new Track[10];
+    private List<Vehicle> competitors = new ArrayList<>();
+
+    public void addCompetitors(int competitorCount) {
+        for (int i = 0; i < competitorCount; i++) {
+            Vehicle vehicle = new Vehicle();
+            // vehicle details will be populated when we learn to get user's input
+            competitors.add(vehicle); // to add an element to the list
+
+        }
+    }
+
+    public void displayCompetitors() {
+        System.out.println("Welcome! Race is starting with:");
+        for (int i = 0; i < competitors.size(); i++) {
+            if (competitors.get(i) != null)
+            System.out.println(competitors.get(i).getName());
+        }
+    }
 
     public void addTrack(int index, Track track) {
         tracks[0] = new Track("Highway", 300);
